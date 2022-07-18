2151dfe57fcd60a11e20c99f22bab0aa56e99910
Shubham Rawat <shubham.rawat@ez.works>
2022-07-18 13:38:43 +0530

    Include dummy API

--- starter/src/App.js
+++ starter/src/App.js
@@ -2,18 +2,32 @@ import React, { useEffect } from 'react'
 import Navbar from './Components/Navbar'
 import CartContainer from './Components/CartContainer'
 import { useSelector, useDispatch } from 'react-redux'
-import { calculateTotals } from './feature/cart/cartSlice'
+import { calculateTotals, getCartItems } from './feature/cart/cartSlice'
 import Modal from './Components/Modal'
 
 const App = () => {
   const dispatch = useDispatch();
-  const {cartItems} = useSelector((state) => state.cart);
+  const {cartItems, isLoading} = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.modal);
 
 
   useEffect(()=>{
     dispatch(calculateTotals());
   },[cartItems])
+
+  useEffect(()=>{
+    dispatch(getCartItems());
+  },[]);
+
+
+  if(isLoading){
+    return(
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
 
   return (
     <main>
